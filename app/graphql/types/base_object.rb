365f2cce7613ff22d2adfb74df98ba2336c51fc5
Tobias Miesel <tobias.miesel@mocoapp.com>
2026-01-13 08:37:16 +0100

    added association field helper

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -1,4 +1,12 @@
 module Types
   class BaseObject < GraphQL::Schema::Object
+    def self.association_field(name, type:, null:, id_field: true, id_type: ID, id_null: null)
+      field :"#{name}_id", id_type, null: id_null if id_field
+      field name, type, null: null
+
+      define_method(name) do
+        dataloader.with(Loaders::AssociationLoader, object.class, name).load(object)
+      end
+    end
   end
 end
--- app/graphql/types/sub_task_type.rb
+++ app/graphql/types/sub_task_type.rb
@@ -1,8 +1,9 @@
 module Types
   class SubTaskType < Types::BaseObject
     field :id, ID, null: false
-    field :task_id, ID, null: false
     field :title, String, null: false
     field :completed, Boolean, null: false
+
+    association_field :task, type: Types::TaskType, null: false
   end
 end
